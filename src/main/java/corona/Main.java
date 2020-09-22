6476f8eff2c4f5d41339f023ab7ebffee9f440e2
KandaTeach <www.keneth.com.ph@gmail.com>
2020-09-23 00:02:22 +0800

    Modified Main Object

---
+++ src/main/java/corona/Main.java
@@ -0,0 +1,35 @@
+package corona;
+
+import corona.parameter.ParameterClass;
+import corona.data.ApiDataSource;
+
+import com.beust.jcommander.JCommander;
+
+import org.json.JSONObject;
+
+/**
+ * API link https://disease.sh/
+ * 
+ * @author John Keneth P. Paluca
+ * @version 1.0.0
+ */
+public class Main {
+
+    private static final ParameterClass PARAMETER_OBJ = new ParameterClass();
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        JCommander.newBuilder().addObject(PARAMETER_OBJ).build().parse(args);
+        if (PARAMETER_OBJ.isHelp())
+            JCommander.newBuilder().addObject(PARAMETER_OBJ).build().usage();
+        else
+            main.run();
+    }
+
+    private void run() {
+        ApiDataSource source = new ApiDataSource(PARAMETER_OBJ.getRequest());
+        String apiUrl = source.apiGetRequest();
+        JSONObject jsonObj = source.responce(apiUrl);
+        source.printResult(jsonObj);
+    }
+}
